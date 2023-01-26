ac8b1a209bbbb5cdb007a2c810d74adcdc12d785
nbela7000 <92248823+nbela7000@users.noreply.github.com>
2023-01-26 15:45:18 +0100

    Create file_new.php

---
+++ file_new.php
@@ -0,0 +1,26 @@
+<div id='fileview_content'>
+<h1>Új akta</h1><br />
+<table id='fileview_table'>
+    <tr>
+        <td><b>Akta sorszáma</b></td>
+        <td><input type='text' id='inputFileName' class='input-primary'></td>
+    </tr>
+    <tr>
+        <td><b>Megnevezés</b></td>
+        <td><input type='text' id='inputTitle' class='input-primary'></td>
+    </tr>
+    <tr>
+        <td colspan='2' style='text-align:center;'><b>Tartalom</b></td>
+    </tr>
+    <tr>
+        <td colspan='2'>
+            <textarea name="editor1" id="editor1" rows="10" cols="80"></textarea><br />
+            <script>CKEDITOR.replace('editor1');CKEDITOR.config.width = '100%';</script>
+        </td>
+    </tr>
+    <tr>
+        <td colspan='2'><a class='button-primary submit-new-file'>Létrehozás</a><span id='newfileInfo' style='margin-left: 10px;'>&nbsp</span></td>
+    </tr>
+</table>
+</div>
+<script src='js/new_file.js'></script>
